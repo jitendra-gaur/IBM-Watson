2285ede540702dfa63b97edeb742332f95ed90f7
Jitendra Gaur <jitendra.gets@gmail.com>
2018-01-15 15:43:15 +0530

    Add files via upload

---
+++ calling_URL.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 11:44:32 2018
+
+@author: Jitendra Gaur
+"""
+
+import urllib2
+import time
+#%%
+i=0
+for i in range(0,365):
+    urllib2.urlopen("https://shanalyticsdatacaiapi.mybluemix.net/start").read()
+    time.sleep(120)
+    
+#%%
